8e2b3a5b43240decac89c049cfbf4dee354344f1
JuliaCMS <julia.cms@outlook.com>
2023-06-05 16:47:11 -0300

    Aula 40

---
+++ Aula40/ProjetoMVC/Controllers/OiMundoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace ProjetoMVC.Controllers
+{
+    public class OiMundoController : Controller
+    {
+        public IActionResult Index()
+        //public string Index()
+        {
+            return View();
+            //return "Apresenta esta mensagem";
+        }
+
+        public string Welcome(string nome, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Oi {nome},ID: {ID}");
+            // https://localhost:7006/OiMundo/Welcome?nome=Julia&vezes=4
+            //return "Estou nométodo Welcome do OiMundoController";
+        }
+
+        public string Oi()
+        {
+            return "Oi";
+        }
+    }
+}
---
+++ Aula40/anotacoes_aula40.md
@@ -0,0 +1,8 @@
+# Anotações aula 40 - 05/06
+
+Aula sobre MVC
+
+Tipot de projeto - Aplicativo web do ASP.NET Core (Model-View-Controller)
+    - pacote que precisa ter instalado: ASP.NET e desenvolvimento web
+
+- Após criar o projetpo clicar pra iniciar sem depuração (ctrl + f5)
