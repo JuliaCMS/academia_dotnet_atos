0f803578c6ecaad6e344deff682238f0dead993c
JuliaCMS <julia.cms@outlook.com>
2023-07-12 02:13:34 -0300

    Novas aulas adicionadas

---
+++ Aula45/anotacoes_aula45.md
@@ -0,0 +1,9 @@
+# Anotações aula 45 - 13/06
+
+LazyProxy (lazingload) -> pontos positivos e negativos ->as vezes é melhor usar o include
+
+comentar o nullable no csproj pra fazer o metodo post funcionar (<!-- <Nullable>enable</Nullable> -->)
+ou definir o parametro como nulo
+
+    asnotracking -> usa quando não vai fazer manipulação dos dados, melhora desempenho
+
---
+++ Aula46/ConsumirAPI/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace ConsumirAPI
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Consumindo a API desenvolvida");
+            int opcao;
+            string BaseUrl = "http://localhost:5010";
+
+            do
+            {
+                Console.WriteLine("Informe a opção desejada:");
+                Console.WriteLine("1 - Consultar pessoas");
+                Console.WriteLine("2 - Cadastrar pessoas");
+                Console.WriteLine("3 - Alterar pessoas");
+                Console.WriteLine("4 - Excluir pessoas");
+                Console.WriteLine("0 - Sair");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch(opcao)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        List<Pessoa> pessoas = new List<Pessoa>(); // receber o que eu tenho armazenado no sistema
+                        HttpClient client = new HttpClient(); // instanciando um objeto para fazer o acesso via http na API
+                        client.BaseAddress = new Uri(BaseUrl); // definindo o endereço da API
+                        client.DefaultRequestHeaders.Clear();
+                        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                        HttpResponseMessage resposta = await client.GetAsync("/Pessoa/pessoas"); // acessando o endpoint da API
+
+                        if(resposta.IsSuccessStatusCode) // obtive sucesso na resposta, entro no if
+                        {
+                            var retorno = resposta.Content.ReadAsStringAsync().Result; // obtenho o retorno de uma consulta a API
+                            pessoas = JsonConvert.DeserializeObject<List<Pessoa>>(retorno); // to pegando o retorno e desserializando e armazenando na lista
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Erro: {resposta.StatusCode.ToString()}");
+                        }
+                        foreach(Pessoa p in pessoas)
+                        {
+                            Console.WriteLine($"ID: {p.id} \nNome: {p.nome}");
+                        }
+                        break;
+                    //case 2:
+                }
+
+            } while(opcao != 0);
+        }
+    }
+}
